0ab565d262b129c417e0415c06250f2a2977889b
Henrik Andersson <henrik@henrikandersson.org>
2020-04-09 16:59:56 +1000

    I'm a right ol' knob...can't spell

--- push/entrypoint.sh
+++ push/entrypoint.sh
@@ -4,38 +4,37 @@ set -e
 function main() {
     echo ""
 
-    if isSet "${INPUTS_SPACE}"; then
-        optionalArgs[0]="--space=${INPUTS_SPACE}"
+    if isSet "${INPUT_SPACE}"; then
+        optionalArgs[0]="--space=${INPUT_SPACE}"
     fi
 
-    if isSet "${INPUTS_OVERWRITE_MODE}"; then
-        optionalArgs[1]="--overwrite-mode=${INPUTS_OVERWRITE_MODE}"
+    if isSet "${INPUT_OVERWRITE_MODE}"; then
+        optionalArgs[1]="--overwrite-mode=${INPUT_OVERWRITE_MODE}"
     fi
 
-    if isFlagSet "${INPUTS_USE_DELTA_COMPRESSION}"; then
+    if isFlagSet "${INPUT_USE_DELTA_COMPRESSION}"; then
         optionalArgs[2]="--use-delta-compression"
     fi
 
-    if isFlagSet "${INPUTS_DEBUG}"; then
+    if isFlagSet "${INPUT_DEBUG}"; then
         optionalArgs[3]="--debug"
     fi
 
-    if isFlagSet "${INPUTS_IGNORE_SSL_ERRORS}"; then
+    if isFlagSet "${INPUT_IGNORE_SSL_ERRORS}"; then
         optionalArgs[4]="--ignoreSslErrors"
     fi
 
-    if isSet "${INPUTS_TIMEOUT}"; then
-        optionalArgs[5]="--timeout=${INPUTS_TIMEOUT}"
+    if isSet "${INPUT_TIMEOUT}"; then
+        optionalArgs[5]="--timeout=${INPUT_TIMEOUT}"
     fi
 
-    if isSet "${INPUTS_LOG_LEVEL}"; then
-        optionalArgs[6]="--logLevel=${INPUTS_LOG_LEVEL}"
+    if isSet "${INPUT_LOG_LEVEL}"; then
+        optionalArgs[6]="--logLevel=${INPUT_LOG_LEVEL}"
     fi
 
-    octo --package=${INPUTS_PACKAGE} \
-         --server=${INPUTS_OCTOPUS_SERVER} \
-         --server=${INPUTS_OCTOPUS_SERVER} \
-         --apiKey=${INPUTS_OCTOPUS_API_KEY} \
+    octo --package=${INPUT_PACKAGE} \
+         --server=${INPUT_OCTOPUS_SERVER} \
+         --apiKey=${INPUT_OCTOPUS_API_KEY} \
          "${optionalArgs[@]}"
 }
 
